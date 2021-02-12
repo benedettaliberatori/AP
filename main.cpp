1888cbd41062016c249a45150030e2e476189d62
Davide <davide.roznowicz@gmail.com>
2021-02-12 11:52:34 +0100

    ok

--- main.cpp
+++ main.cpp
@@ -67,25 +67,32 @@ int main(){
     
     bst<k_type,v_type> tree{};
     std::string s = "ciao";
-    std::pair<k_type,v_type> x{3,s};
-    std::pair<k_type,v_type> k{4,"monte"};
-    std::pair<k_type,v_type> z{5,s};
-    std::pair<k_type,v_type> b{18349,s};
-    std::pair<k_type,v_type> v{4,s};  // same key==4 not added by insert !
+    std::pair<k_type,v_type> x{100,s};
+    std::pair<k_type,v_type> k{8,"monte"};
+    std::pair<k_type,v_type> z{9,s};
+    std::pair<k_type,v_type> b{10,s};
+    std::pair<k_type,v_type> v{12,s};
+    std::pair<k_type,v_type> f{11,s};
     
     tree.insert(x);
     tree.insert(k);    
     tree.insert(z);
-    //tree.insert(b);
+    tree.insert(b);
     tree.insert(v);
+    tree.insert(f);
     std::cout << tree;
     tree.print_structure();
+    tree.balance();
+    tree.print_structure();
+    
+    
+    
     /*
-    for (int i=0; i<1000; i++) {
+    for (int i=0; i<100; i++) {
     		std::pair<k_type,v_type> bb{i,s};
     		tree.insert(bb);
     }
-    
+    */
     k_type key = 989;
     // measure time of finding a key
     auto start = high_resolution_clock::now();
@@ -98,13 +105,16 @@ int main(){
     auto duration = duration_cast<microseconds>((stop - start)/1000);
     std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
     // end measure
+    tree.print_structure();
+    tree.balance();
+    tree.print_structure();
     tree.find(123);
     
     std::cout << "**************************** tree[" << key << "] = " << tree[key] << std::endl;
     
     bst<k_type,v_type> t{};   // custom ctor
     bst<k_type,v_type> p{tree};  // copy ctor - deepcopy
-    std::cout << p;	  // print copied tree
+    //std::cout << p;	  // print copied tree
 
-  */
+  
 }
