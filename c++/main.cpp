e0826eae48f520ab6219ed7716188a30defa06de
ciroantonio <mamiciroantonio@gmail.com>
2021-02-15 09:17:26 +0100

    Minor changes

--- c++/main.cpp
+++ c++/main.cpp
@@ -76,34 +76,5 @@ int main(){
    
 
 
-/*
-
-    using k_type = const int;     // set type of key
-    using v_type = std::string;   // set type of value
-    
-    bst<k_type,v_type> tree{};
-    std::string s = "ciao";
-    std::pair<k_type,v_type> x{3,s};
-    std::pair<k_type,v_type> k{4,"monte"};
-    std::pair<k_type,v_type> z{5,s};
-    std::pair<k_type,v_type> b{18349,s};
-    std::pair<k_type,v_type> v{4,s};  // same key==4 not added by insert !
-    tree.insert(k);
-  
-    srand(1);
-    auto start = high_resolution_clock::now();
-    for (int i=0; i<100000; i++) {
-        const int j = rand() % 100000 + 1;
-        std::pair<k_type,v_type> bb{j,s};
-        tree.insert(bb);
-    }
-   
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
-    // end measure
-
-*/
-
   
 }
